977863444a6da5da6a75fda9a0e527adbfbeb865
Thiago Simonin <thiago.simonin@gmail.com>
2013-03-01 12:36:54 -0300

    Exemplo de constantes

---
+++ constantes.php
@@ -0,0 +1,26 @@
+<?php
+
+define ('CONSTANTE', 'Ola Mundo', true);
+// define ('constante', 'Hello World', false); false e o valor default
+
+echo CONSTANTE;
+echo '<br>';
+echo constante;
+echo '<br>';
+echo CoNsTaNtE;
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+echo __FILE__;
+echo ':';
+echo __LINE__;
+
+  /*
+   * __FUNCTION__
+   * __CLASS__
+   * __METHOD__
+   *
+   */
+
