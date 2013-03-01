7665b03b5aef0512d981e7e1fc2df3bd7498db27
Thiago Simonin <thiago.simonin@gmail.com>
2013-03-01 01:18:43 -0300

    Exemplos de variveis

--- info.php
+++ info.php
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-<?php
-
-phpinfo();
-?>
-=======
-<?php phpinfo(); ?>
->>>>>>> ddaa180273425fb0a43246d88154e9a2e49015e1
+<?php phpinfo();
---
+++ variavel.php
@@ -0,0 +1,15 @@
+<?php
+
+$nome_de_variavel;
+$nomeDeVariavel;
+$numero13;
+$_13;
+
+$Usuario = 'simonin';
+$usuario = 'thiago';
+
+echo $usuario;
+echo '<br>';
+echo $Usuario;
+
+?>
