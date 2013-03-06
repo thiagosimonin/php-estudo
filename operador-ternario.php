8f078ae0c6fda9b006e5afa88115eb8134dc486c
Thiago Simonin <thiago.simonin@gmail.com>
2013-03-06 00:51:52 -0300

    Operador ternario

---
+++ operador-ternario.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$acesso = 1;
+echo 'usuário' . "\n" ;
+echo ($acesso > 0) ? 'registrado' : 'não registrado';
+
+echo '<br><hr><br>';
+
+if($acesso > 0){
+    echo 'usuário registrado';
+}else{
+    echo 'usuário não registrado';
+}
